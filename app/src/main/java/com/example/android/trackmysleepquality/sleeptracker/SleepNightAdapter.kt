4b00cf81aed603aa1c89b204770c43b6f8c38e8f
Johnny Cheng <johnny.cheng7b9@gmail.com>
2019-10-01 08:00:56 -0700

    Step 09 Use BindingAdapter
    - Implemented BindingUtil and refactor formatting from SleepNightAdapter
    - Use @BindingAdapter annotation
    - Bind sleep variables in list_item_sleep_night.xml

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -1,6 +1,5 @@
 package com.example.android.trackmysleepquality.sleeptracker
 
-import android.content.res.Resources
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -34,34 +33,18 @@ class SleepNightAdapter: ListAdapter<SleepNight, SleepNightAdapter.ViewHolder>(S
 //    override fun getItemCount(): Int = data.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-//        val item = data[position]
         val item = getItem(position)
         Log.i(TAG, "Setting new data: ${item.sleepQuality.toString()}")
 
-        val res = holder.itemView.context.resources
-        holder.bind(holder, item, res)
+        holder.bind(item)
     }
 
-    class ViewHolder private constructor(binding: ListItemSleepNightBinding): RecyclerView.ViewHolder(binding.root) {
+    class ViewHolder private constructor(val binding: ListItemSleepNightBinding): RecyclerView.ViewHolder(binding.root) {
 
-        val sleepLength: TextView = binding.sleepLength
-        val quality: TextView = binding.qualityString
-        val qualityImage: ImageView = binding.qualityImage
 
-        fun bind(holder: ViewHolder, item: SleepNight, res: Resources) {
-            holder.sleepLength.text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
-            holder.quality.text = convertNumericQualityToString(item.sleepQuality, res)
-            holder.qualityImage.setImageResource(
-                    when (item.sleepQuality) {
-                        0 -> R.drawable.ic_sleep_0
-                        1 -> R.drawable.ic_sleep_1
-                        2 -> R.drawable.ic_sleep_2
-                        3 -> R.drawable.ic_sleep_3
-                        4 -> R.drawable.ic_sleep_4
-                        5 -> R.drawable.ic_sleep_5
-                        else -> R.drawable.ic_sleep_active
-                    }
-            )
+        fun bind(item: SleepNight) {
+            binding.sleep = item
+            binding.executePendingBindings()
         }
 
         // Use companion object to self initialize
